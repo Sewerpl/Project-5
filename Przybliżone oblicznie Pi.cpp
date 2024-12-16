efc4c8ca96337b64deda2a1eb0d39925b480aaa5
PC <seweryn.contact@gmail.com>
2024-12-16 16:27:42 +0100

    Funkcja obliczeniowa wykonywana przez wątki

--- Przybliżone oblicznie Pi.cpp
+++ Przybliżone oblicznie Pi.cpp
@@ -12,6 +12,19 @@ struct ThreadData {
     double partial_sum;
 };
 
+// Funkcja obliczeniowa wykonywana przez wątki
+void* calculate_partial_sum(void* arg) {
+    ThreadData* data = (ThreadData*)arg;
+    double sum = 0.0;
+
+    for (long i = data->start; i < data->end; ++i) {
+        double x = (i + 0.5) * data->step;
+        sum += sqrt(1.0 - x * x);
+    }
+
+    data->partial_sum = sum;
+    return nullptr;
+}
 
 
     return 0;
